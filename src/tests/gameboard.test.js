22b6532ae5184f6485d7748e3f534a2bb1d82cef
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-30 19:18:19 +0200

    Add tests for ship positioning on board

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,5 +3,26 @@ import Gameboard from '../classes/gameboard.js'
 const deepSea = new Gameboard;
 
 describe('Gameboard', () => {
-    it('')
-})
+    describe('Ship placing', () => {
+        beforeEach(() => {
+            deepSea.placeShip(['a1', 'a2', 'a3']);
+        });
+            it('Battleship placing', () => {
+                expect(deepSea.docks).toEqual([
+                    {
+                        length: 3,
+                        hitPoints: 3,
+                        coordinates: ['a1', 'a2', 'a3']
+                    }
+                ]);
+            });
+
+            it('Trying to place destroyer on occupied space', () => {
+                expect(deepSea.placeShip(['a1', 'b1'])).toBeFalsy;
+            });
+            
+        afterEach(() => {
+            deepSea.docks = [];
+        })
+    });
+});
