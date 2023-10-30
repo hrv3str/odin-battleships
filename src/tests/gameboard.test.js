46e79b4e126174c4c8621d9b045af87c2ea54422
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-30 20:41:29 +0200

    Add test cases for receiveAttack method

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,15 +3,14 @@ import Gameboard from '../classes/gameboard.js'
 const deepSea = new Gameboard;
 
 describe('Gameboard', () => {
+    deepSea.placeShip(['a1', 'a2', 'a3']);
+
     describe('Ship placing', () => {
-        beforeEach(() => {
-            deepSea.placeShip(['a1', 'a2', 'a3']);
-        });
             it('Battleship placing', () => {
                 expect(deepSea.docks).toEqual([
                     {
                         length: 3,
-                        hitPoints: 3,
+                        hitPoints: 2,
                         coordinates: ['a1', 'a2', 'a3']
                     }
                 ]);
@@ -20,9 +19,25 @@ describe('Gameboard', () => {
             it('Trying to place destroyer on occupied space', () => {
                 expect(deepSea.placeShip(['a1', 'b1'])).toBeFalsy;
             });
-            
-        afterEach(() => {
-            deepSea.docks = [];
-        })
     });
+
+    describe('Attack', () => {
+        deepSea.receiveAttack('a2');
+        it('Attack on ship', () => {
+            expect(deepSea.docks[0].hitPoints).toBe(2);
+            expect(deepSea.hits).toEqual(['a2']);
+            expect(deepSea.receiveAttack('a3')).toBeTruthy;
+        });
+
+        deepSea.receiveAttack('c3');
+        it('Miss attack', () => {
+            expect(deepSea.misses).toEqual(['c3']);
+            expect(deepSea.receiveAttack('c4')).toBe(null);
+        });
+
+        it('Attack on the used coordinates', () => {
+            expect(deepSea.receiveAttack('c4')).toBeFalsy;
+            expect(deepSea.receiveAttack('a2')).toBeFalsy;
+        });
+    })
 });
