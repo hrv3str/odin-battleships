a0eeb16e0997d9c71939a42b5471be74c93146db
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-30 20:40:41 +0200

    Implement .receiveAttack() method logic

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -15,6 +15,7 @@ export default class Gameboard {
         this.docks.push(ship);
     }
 
+    // Iterates through all 'coordinates' keys of Ships stored in docks to find if given coordinates are not occupied
     checkDocks (coordinatesArray) {
         coordinatesArray.forEach(coordinate => {
             this.docks.forEach(ship => {
@@ -26,4 +27,31 @@ export default class Gameboard {
 
         return true;
     }
+
+    // Gets coordinates in the string type
+    receiveAttack (string) {
+        // Check if coordinates for attack are avaible
+        for (const item of this.hits) {
+            if (item === string) return false;
+        };
+
+        for (const item of this.misses) {
+            if (item === string) return false
+        };
+
+        // Checks if coordinates are belong to any of ships
+        for (const ship of this.docks) {
+            for (const item of ship.coordinates) {
+                if (item === string) {
+                    ship.hit();
+                    this.hits.push(string);
+                    return true;
+                };
+            };
+        };
+
+        // if not found in ships then coordinates stored in misses
+        this.misses.push(string);
+        return null
+    };
 }
