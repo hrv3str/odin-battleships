a2cfc310322b7c86be389a3fce0ed2c3331d5937
hrv3str <r.novikov.zp.ua@gmail.com>
2023-11-02 23:44:14 +0200

    Develop and add .placeShip() method

--- src/classes/UI.js
+++ src/classes/UI.js
@@ -1,6 +1,10 @@
 class UI {
     constructor() {
         this.body = document.body;
+        this.gameboardLeft = undefined;
+        this.gameboardRight = undefined;
+        this.cellsLeft = undefined;
+        this.cellsRight = undefined;
     }
 
     start () {
@@ -47,8 +51,8 @@ class UI {
         `
         this.body.appendChild(plate);
 
-        const gameboardLeft = document.getElementById('gameboard-left');
-        const gameboardRight = document.getElementById('gameboard-right');
+        this.gameboardLeft = document.getElementById('gameboard-left');
+        this.gameboardRight = document.getElementById('gameboard-right');
 
         const fillGameboard = (gameboard) => {
             const createCell = () => {
@@ -67,8 +71,8 @@ class UI {
             let number = 1;
 
             for (let i = 0; i < cellList.length; i++) {
-                const coordinate = letter + number;
-                cellList[i].dataset.coordinate = coordinate;
+                cellList[i].dataset.y = letter;
+                cellList[i].dataset.x = number;
 
                 number++
 
@@ -79,8 +83,73 @@ class UI {
             }
         }
 
-        fillGameboard(gameboardLeft);
-        fillGameboard(gameboardRight);
+        fillGameboard(this.gameboardLeft);
+        fillGameboard(this.gameboardRight);
+        this.cellsLeft = this.gameboardLeft.querySelectorAll('.cell');
+        this.cellsRight = this.gameboardRight.querySelectorAll('.cell');
+    }
+
+    searchCell (x, y) {
+        for (let i = 0; i < this.cellsLeft.length; i++) {
+            const cell = this.cellsLeft[i];
+            if (cell.dataset.x === x && cell.dataset.y === y) return cell;
+        }
+    }
+
+    placeBattleship (shipLength) {
+        let rotation = 'vertical'
+        const length = shipLength;
+
+        const rotate = (e) => {
+            if (e.key === 'r' || e.key === 'R') {
+                if (rotation === 'horizontal') rotation = 'vertical';
+                else rotation = 'horizontal';
+            } else return
+        }
+
+        const shadowShip = (e) => {
+            const target = e.target;
+
+            const template = []
+
+            switch (rotation) {
+                case 'horizontal':
+                    for (let i = 0; i < length; i++) {
+                        const x = (parseInt(target.dataset.x) - length + 1 + i).toString();
+                        const cell = this.searchCell(x, target.dataset.y);
+                        template.push(cell);
+                    }
+                    break;
+                case 'vertical':
+                    if(!target.dataset.y) return;
+                    for (let i = 0; i < length; i++) {
+                        const y = String.fromCharCode(target.dataset.y.charCodeAt(0) - shipLength + 1 + i);
+                        if (y < 'a') return;
+                        const cell = this.searchCell(target.dataset.x, y);
+                        template.push(cell);
+                      }
+                    break;
+            }
+            
+            const unHover = () => {
+                template.forEach(item => {
+                    if (item !== undefined && item.classList.contains('hovered')) {
+                        item.classList.remove('hovered');
+                    }
+                })
+            }
+
+            template.forEach(item => {
+                if (item !== undefined && !item.classList.contains('gameboard')) {
+                    item.classList.add('hovered');
+                }
+            })
+
+            target.addEventListener('mouseleave', unHover);
+        }
+
+        this.gameboardLeft.addEventListener('mouseover', shadowShip);
+        document.addEventListener('keydown', rotate)
     }
 }
 
