39c1780458ec3919d29022b48c52638c9b6c4140
hrv3str <r.novikov.zp.ua@gmail.com>
2023-11-03 22:24:03 +0200

    Add .surroundWithMisses() method

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -28,6 +28,98 @@ export default class Gameboard {
         return true;
     }
 
+    // Takes ship coordinates and calculates surrounding coordinates to add to misses array
+    surroundWithMisses (ship) {
+        // Takes coordinates from ship objject
+        const source = ship.coordinates;
+
+        // Iterates trough ship coordinates
+        for (const item of source) {
+            // deconstruct string to ease operations
+            const [letter, number] = item;
+            // converts string to number
+            const num = parseInt(number);
+
+            // Calculate the set of coordinates, checking if they are an boundaries
+            const left = () => {
+                if (num - 1 >= 1) {
+                    return`${letter}${num - 1}`
+                };
+            };
+
+            const right = () => {
+                if (num + 1 <= 10) {
+                    return`${letter}${num + 1}`
+                };
+            };
+
+            const above = () => {
+                const delta = letter.charCodeAt(0) - 1;
+                if (String.fromCharCode(delta) >= 'a') {
+                    return String.fromCharCode(delta) + number;
+                };
+            };
+
+            const aboveLeft = () => {
+                const deltaY = letter.charCodeAt(0) - 1;
+                const deltaX = num - 1;
+                if (deltaX >= 1 && String.fromCharCode(deltaY) >= 'a') {
+                    return String.fromCharCode(deltaY) + deltaX;
+                }
+            }
+
+            const aboveRight = () => {
+                const deltaY = letter.charCodeAt(0) - 1;
+                const deltaX = num + 1;
+                if (deltaX <= 10 && String.fromCharCode(deltaY) >= 'a') {
+                    return String.fromCharCode(deltaY) + deltaX;
+                }
+            }
+
+            const below = () => {
+                const delta = letter.charCodeAt(0) + 1
+                if (String.fromCharCode(delta) <= 'j') {
+                    return String.fromCharCode(delta) + number;
+                }
+            };
+
+            const belowLeft = () => {
+                const deltaY = letter.charCodeAt(0) + 1;
+                const deltaX = num - 1;
+                if (String.fromCharCode(deltaY) <= 'j' && deltaX >= 1) {
+                    return String.fromCharCode(deltaY) + deltaX;
+                }
+            }
+
+            const belowRight = () => {
+                const deltaY = letter.charCodeAt(0) + 1;
+                const deltaX = num + 1;
+                if (String.fromCharCode(deltaY) <= 'j' && deltaX <= 10) {
+                    return String.fromCharCode(deltaY) + deltaX;
+                }
+            }
+
+            // Form output
+            const output = [
+                    aboveLeft(),
+                    above(),
+                    aboveRight(),
+                    right(),
+                    belowRight(),
+                    below(),
+                    belowLeft(),
+                    left()
+            ]
+
+            // Check if output items are not used before of are not 'undefined' and push them into misses
+            output.forEach(item => {
+                if (!this.misses.includes(item) && !source.includes(item) && item !== undefined) {
+                    this.misses.push(item);
+                };
+            });
+        };
+    };
+
     // Gets coordinates in the string type
     receiveAttack (string) {
         // Check if coordinates for attack are avaible
@@ -45,6 +137,7 @@ export default class Gameboard {
                 if (item === string) {
                     ship.hit();
                     this.hits.push(string);
+                    if (ship.isSunk()) surroundWithMisses(ship);
                     return true;
                 };
             };
