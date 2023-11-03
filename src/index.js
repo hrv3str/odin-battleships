256b954d6961998b19ae4b94d9160a4a37644d7d
hrv3str <r.novikov.zp.ua@gmail.com>
2023-11-03 22:26:16 +0200

    Add .ship-body, .miss and .hit classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import 'normalize.css';
 import './styles.css';
-import UI from './classes/UI';
+import UI from './classes/UI.js';
+import Gameboard from './classes/gameboard.js';
 
+const gameboardLeft = new Gameboard;
+const gameboardRight = new Gameboard;
 const ui = new UI;
 
-ui.game();
-ui.placeBattleship(1);
+ui.linkSources(gameboardLeft, gameboardRight);
+
+ui.gameScreen();
+
+const battleship = ['b2', 'b3', 'b4'];
+const aircraftCarrier = ['b9', 'c9', 'd9', 'e9'];
+const patrolBoat = ['j1'];
+const destroyer = ['f5', 'e5'];
+gameboardLeft.placeShip(battleship);
+gameboardLeft.placeShip(aircraftCarrier);
+gameboardLeft.placeShip(patrolBoat);
+gameboardLeft.placeShip(destroyer);
+gameboardLeft.receiveAttack('b3');
+gameboardLeft.receiveAttack('b5');
+
+ui.refreshBoard();
