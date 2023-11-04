357e954bfbcb3c1f5baa3c05b4a2b06f7ad9b309
hrv3str <r.novikov.zp.ua@gmail.com>
2023-11-04 21:39:15 +0200

    Test UI module

--- src/index.js
+++ src/index.js
@@ -10,16 +10,3 @@ const ui = new UI;
 ui.linkSources(gameboardLeft, gameboardRight);
 
 ui.gameScreen();
-
-const battleship = ['b2', 'b3', 'b4'];
-const aircraftCarrier = ['b9', 'c9', 'd9', 'e9'];
-const patrolBoat = ['j1'];
-const destroyer = ['f5', 'e5'];
-gameboardLeft.placeShip(battleship);
-gameboardLeft.placeShip(aircraftCarrier);
-gameboardLeft.placeShip(patrolBoat);
-gameboardLeft.placeShip(destroyer);
-gameboardLeft.receiveAttack('b3');
-gameboardLeft.receiveAttack('b5');
-
-ui.refreshBoard();
